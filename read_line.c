ea4187562dee6c9af3a19fce322461b5f46be8e9
Beydi-dev <beydi.coulibalyy@gmail.com>
2025-04-15 18:40:52 +0200

    adding malloc and free in read_line folder

--- read_line.c
+++ read_line.c
@@ -9,6 +9,11 @@ char *str = NULL;
 size_t size = 0;
 ssize_t line;
 (void)ac;
+//const char *delim = " \n";
+char *token;
+int count = 0;
+int i = 0;
+char *str_save;
 
 /* boucle infinie*/
 while (1)
@@ -22,6 +27,41 @@ while (1)
 		return (-1);
 	}
 	printf("%s", str);
+
+	count = 0;
+	i = 0;
+/*on travaille sur une copie de str car strtok modifie la chaîne*/
+/*on compte le nombre de caracteres dans str pour le malloc dans av*/
+	str_save = strdup(str);
+	token = strtok(str_save, " \n");
+	while (token != NULL)
+	{
+		count++;
+		token = strtok(NULL, " \n");
+	}
+/*allocation dans av maintenant qu'on connait count, +1 pour NULL byte*/
+	av = malloc(sizeof(char *) * (count + 1));
+
+	if (av == NULL)
+	{
+		return (-1);
+	}
+	token = strtok(str_save, " \n");
+
+	while (token != NULL)
+	{
+		av[i] = strdup(token);
+		i++;
+		token = strtok(NULL, " \n");
+	}
+	av[i] = NULL;
+	for (i = 0; av[i] != NULL; i++)
+	{
+		free(av[i]);
+	}
+	free(av);
+	free(str_save);
 }
+free(str);
 return (0);
 }
