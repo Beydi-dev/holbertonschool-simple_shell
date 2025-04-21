04173fc4b0fdabc30b63cb861393fb6559761841
Beydi-dev <beydi.coulibalyy@gmail.com>
2025-04-21 17:15:36 +0200

    deleting unused variables in super_simple_shell

--- super_simple_shell.c
+++ super_simple_shell.c
@@ -2,41 +2,41 @@
 
 /**
  * main - Entry point of the super simple shell
- *
+ * @ac: Argument count
+ * @av: Argument vector
  * Return: Always 0 on success
  */
 int main(int ac, char **av)
 {
 	char *line = NULL;
 	size_t len = 0;
-	char **argv = NULL;
+	char **argv, *shell_name = av[0];
 	pid_t pid;
-	int status;
-	int line_number = 0;
-	char *shell_name = av[0];
+	int status, line_number = 1;
 	(void)ac;
 
 	while (1)
 	{
 		if (read_line(&line, &len) == -1)
-		{
-			free(line);
 			break;
-		}
 
 		argv = tokenize_line(line);
 		if (!argv || !argv[0])
 		{
 			free_argv(argv);
+			line_number++;
 			continue;
 		}
-
 		pid = fork();
 		if (pid == 0)
 		{
 			if (execve(argv[0], argv, environ) == -1)
 			{
-				fprintf(stderr, "%s: %d: %s: not found\n", shell_name, line_number, argv[0]);
+				fprintf(stderr,
+						"%s: %d: %s: not found\n",
+						shell_name,
+						line_number,
+						argv[0]);
 				exit(127);
 			}
 		}
@@ -44,6 +44,7 @@ int main(int ac, char **av)
 			wait(&status);
 
 		free_argv(argv);
+		line_number++;
 	}
 	free(line);
 	return (0);
