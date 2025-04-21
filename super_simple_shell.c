b1449abe8afc338134521a3100758920f60786f5
Beydi-dev <beydi.coulibalyy@gmail.com>
2025-04-21 21:16:56 +0200

    editing handling error condition super_simple_shell file

--- super_simple_shell.c
+++ super_simple_shell.c
@@ -1,23 +1,36 @@
 #include "main.h"
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/types.h>
 
 /**
  * main - Entry point of the super simple shell
  * @ac: Argument count
  * @av: Argument vector
- * Return: Always 0 on success
+ * @envp: Environment variables
+ * Return: 0 on success
  */
-int main(int ac, char **av)
+int main(int ac, char **av, char **envp)
 {
 	char *line = NULL;
 	size_t len = 0;
-	char **argv, *shell_name = av[0];
+	ssize_t nread;
+	char **argv;
 	pid_t pid;
 	int status, line_number = 1;
+
 	(void)ac;
 
 	while (1)
 	{
-		if (read_line(&line, &len) == -1)
+		if (isatty(STDIN_FILENO))
+			printf("$ ");
+
+		nread = getline(&line, &len, stdin);
+		if (nread == -1)
 			break;
 
 		argv = tokenize_line(line);
@@ -27,16 +40,13 @@ int main(int ac, char **av)
 			line_number++;
 			continue;
 		}
+
 		pid = fork();
 		if (pid == 0)
 		{
-			if (execve(argv[0], argv, environ) == -1)
+			if (execve(argv[0], argv, envp) == -1)
 			{
-				fprintf(stderr,
-						"%s: %d: %s: not found\n",
-						shell_name,
-						line_number,
-						argv[0]);
+				fprintf(stderr, "%s: %d: %s: not found\n", av[0], line_number, argv[0]);
 				exit(127);
 			}
 		}
@@ -46,6 +56,7 @@ int main(int ac, char **av)
 		free_argv(argv);
 		line_number++;
 	}
+
 	free(line);
 	return (0);
 }
